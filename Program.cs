ffe8de6da08d18f187d7d6b995ff1e65018058eb
Michael <michael.filler@dahag.de>
2022-01-28 12:48:02 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json.Nodes;
+using AutoMapper;
+using AutomapperTest;
+using Newtonsoft.Json.Linq;
+
+Console.WriteLine("Hello, World!");
+
+var config = new MapperConfiguration(config =>
+{
+    config.CreateMap<Test, TestViewModel>();
+});
+
+var mapper = new Mapper(config);
+
+var mode1l = new JObject { { "key", "value" } };
+
+var model = new Test
+{
+    Testi = mode1l
+};
+
+var result = mapper.Map<TestViewModel>(model);
+
+
+Console.WriteLine(result.Testi["key"]);
---
+++ Test.cs
@@ -0,0 +1,13 @@
+using Newtonsoft.Json.Linq;
+
+namespace AutomapperTest;
+
+public class TestViewModel
+{
+    public JObject Testi;
+}
+
+public class Test
+{
+    public JObject Testi;
+}
